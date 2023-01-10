dcd1a2ae8e5f4a77e840a918d9561c53def595b0
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2023-01-10 09:55:01 -0500

    Update robot-config.h

--- include/robot-config.h
+++ include/robot-config.h
@@ -1,1 +1,32 @@
+using namespace vex;
 
+extern brain Brain;
+
+////////////////////////////
+//
+// VEXcode devices
+//
+////////////////////////////////////////////////
+
+// sensors
+
+// motors
+extern motor lfMotor;
+extern motor rfMotor;
+extern motor lbMotor;
+extern motor rbMotor;
+
+// motor_group
+extern motor_group LeftDriveSmart;
+extern motor_group RightDriveSmart;
+
+
+// drivetrain
+extern drivetrain Drivetrain;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
