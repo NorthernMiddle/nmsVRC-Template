1f6a16c07f11322c244aeff5a8dfa176bcc8b8a9
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2023-01-10 10:03:46 -0500

    Update auton.cpp

--- src/routines/auton.cpp
+++ src/routines/auton.cpp
@@ -1,1 +1,141 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       auton.cpp                                                 */
+/*    Author:       NMS_RP                                                    */
+/*    Created:      03 Dec 2019                                               */
+/*    Description:  15-Second Autonomous Routines                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+using namespace vex;
+
+// -----------------------------------------------------------------------------
+// @brief      Red Alliance Auton Program #1                                           
+// -----------------------------------------------------------------------------
+void red_auton1() 
+{
+  // DESCIPTION/PURPOSE OF PROGRAM: ???
+  Brain.Screen.newLine();
+  Brain.Screen.setCursor(1,1);
+  Brain.Screen.print("Red Alliance Auton Program #1 is running"); 
+
+  // ... insert autonomous program here
+
+  // don't hog cpu; allow other programs to run
+  task::sleep(500);  
+}
+
+// -----------------------------------------------------------------------------
+// @brief      Red Alliance Auton Program #2                                           
+// -----------------------------------------------------------------------------
+void red_auton2() 
+{
+  // DESCIPTION/PURPOSE OF PROGRAM: ???
+  Brain.Screen.newLine();
+  Brain.Screen.setCursor(1,1);
+  Brain.Screen.print("Red Alliance Auton Program #2 is running"); 
+
+  // ... insert autonomous program here
+
+  // don't hog cpu; allow other programs to run
+  task::sleep(500); 
+} 
+
+// -----------------------------------------------------------------------------
+// @brief      Red Alliance Auton Program #3                                           
+// -----------------------------------------------------------------------------
+void red_auton3() 
+{
+  // DESCIPTION/PURPOSE OF PROGRAM: ???
+  Brain.Screen.newLine();
+  Brain.Screen.setCursor(1,1);
+  Brain.Screen.print("Red Alliance Auton Program #3 is running"); 
+
+  // ... insert autonomous program here
+
+  // don't hog cpu; allow other programs to run
+  task::sleep(500);   
+}
+
+// -----------------------------------------------------------------------------
+// @brief      Red Alliance Auton Program #4                                           
+// -----------------------------------------------------------------------------
+void red_auton4() 
+{
+  // DESCIPTION/PURPOSE OF PROGRAM: ???
+  Brain.Screen.newLine();
+  Brain.Screen.setCursor(1,1);
+  Brain.Screen.print("Red Alliance Auton Program #4 is running"); 
+
+  // ... insert autonomous program here
+
+  // don't hog cpu; allow other programs to run
+  task::sleep(500);    
+}
+
+// -----------------------------------------------------------------------------
+// @brief      Blue Alliance Auton Program #1                                         
+// -----------------------------------------------------------------------------
+void blue_auton1() 
+{
+  // DESCIPTION/PURPOSE OF PROGRAM: ???
+  Brain.Screen.newLine();
+  Brain.Screen.setCursor(1,1);
+  Brain.Screen.print("Blue Alliance Auton Program #1 is running"); 
+
+  // ... insert autonomous program here
+
+  // don't hog cpu; allow other programs to run
+  task::sleep(500);    
+}
+
+
+// -----------------------------------------------------------------------------
+// @brief      Blue Alliance Auton Program #2                                         
+// -----------------------------------------------------------------------------
+void blue_auton2() 
+{
+  // DESCIPTION/PURPOSE OF PROGRAM: ???
+  Brain.Screen.newLine();
+  Brain.Screen.setCursor(1,1);
+  Brain.Screen.print("Blue Alliance Auton Program #2 is running"); 
+
+  // ... insert autonomous program here
+
+  // don't hog cpu; allow other programs to run
+  task::sleep(500);    
+}
+
+// -----------------------------------------------------------------------------
+// @brief      Blue Alliance Auton Program #3                                         
+// -----------------------------------------------------------------------------
+void blue_auton3() 
+{
+  // DESCIPTION/PURPOSE OF PROGRAM: ???
+  Brain.Screen.newLine();
+  Brain.Screen.setCursor(1,1);
+  Brain.Screen.print("Blue Alliance Auton Program #3 is running"); 
+
+  // ... insert autonomous program here
+
+  // don't hog cpu; allow other programs to run
+  task::sleep(500);    
+}
+
+// -----------------------------------------------------------------------------
+// @brief      Blue Alliance Auton Program #4                                         
+// -----------------------------------------------------------------------------
+void blue_auton4() 
+{
+  // DESCIPTION/PURPOSE OF PROGRAM: ???
+  Brain.Screen.newLine();
+  Brain.Screen.setCursor(1,1);
+  Brain.Screen.print("Blue Alliance Auton Program #4 is running"); 
+
+  // ... insert autonomous program here
+
+  // don't hog cpu; allow other programs to run
+  task::sleep(500);    
+}
 
