87956794db0a50e14c0dd37a138734f81588a96f
Reece Turner <43293575+nmsMentor@users.noreply.github.com>
2023-01-11 00:24:18 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
-
 using namespace vex;
 
 // A global instance of competition
@@ -19,17 +18,20 @@ competition Competition;
 
 // define your global instances of motors and other devices here
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
 
-void pre_auton(void) {
+///////////////////////////////////////////////////////////////////////////////
+//
+//  PRE-AUTON FUNCTIONS
+//
+//  You may want to perform some actions before the competition starts. 
+//  Do them in the following function.  You must return from this function or 
+//  the autonomous and usercontrol tasks will not be started. This function 
+//  is only called once after the V5 has been powered on and not every time 
+//  that the robot is disabled. 
+//
+//////////////////////////////////////////////////////////////////////////////
+void pre_auton( void ) 
+{
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
@@ -37,35 +39,39 @@ void pre_auton(void) {
   // Example: clearing encoders, setting servo positions, ...
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
-void autonomous(void) {
+//////////////////////////////////////////////////////////////////////////////
+//
+//  AUTONOMOUS TASK
+//
+//  This task is used to control your robot during the autonomous phase of 
+//  a VEX Competition.
+//
+//  You must modify the code to add your own robot specific commands here. 
+//
+///////////////////////////////////////////////////////////////////////////////
+void autonomous(void) 
+{
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
 
-void usercontrol(void) {
+////////////////////////////////////////////////////////////////////////////////
+//
+//  USER CONTROL TASKS
+//
+//  This task is used to control your robot during the user control phase of
+//  a VEX Competition. 
+//
+//  You must modify the code to add your own robot specific commands here. 
+//
+///////////////////////////////////////////////////////////////////////////////
+void usercontrol( void ) 
+{
   // User control code here, inside the loop
-  while (1) {
+  while ( true ) 
+  {
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
@@ -80,9 +86,14 @@ void usercontrol(void) {
   }
 }
 
+
+////////////////////////////////////////////////////////////////
 //
-// Main will set up the competition functions and callbacks.
+//  MAIN
 //
+//  Main will set up the competition functions and callbacks.
+//
+///////////////////////////////////////////////////////////////////////////
 int main() 
 {
   // Set up callbacks for autonomous and driver control periods.
@@ -93,8 +104,11 @@ int main()
   pre_auton();
 
   // Prevent main from exiting with an infinite loop.
-  while (true) 
+  while ( true ) 
   {
     wait(100, msec);
   }
+  
 }
+
+
